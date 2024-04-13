5233d5bfafa90d8186e844e4c4924d16e0c0df3a
valdez7900 <lave790@gmail.com>
2024-04-13 13:53:09 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gdg-ionic-demo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
